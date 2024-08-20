66ad73e6737dae800dd07999424d627f7440a9d1
lolmaosteffen <steffen-1997@live.no>
2024-08-20 14:35:57 +0200

    finished

--- code.js
+++ code.js
@@ -1,10 +1,16 @@
 const display = document.getElementById("display");
+const lapsContainer = document.getElementById("laps");
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
 
 function start() {
+    document.getElementById("controls").innerHTML = /*HTML*/`
+    <button id="stopBtn" onclick="stop()">stop</button>
+    <button id="lapBtn" onclick="doLap()">lap</button>
+    <button id="resetBtn" onclick="reset()">reset</button>
+    `;
     if (!isRunning) {
         startTime = Date.now() - elapsedTime
         timer = setInterval(update, 10);
@@ -12,6 +18,11 @@ function start() {
     }
 }
 function stop() {
+    document.getElementById("controls").innerHTML = /*HTML*/`
+    <button id="startBtn" onclick="start()">start</button>
+    <button id="stopBtn" onclick="stop()">stop</button>
+    <button id="resetBtn" onclick="reset()">reset</button>
+    `;
     if (isRunning) {
         clearInterval(timer);
         elapsedTime = Date.now() - startTime;
@@ -19,11 +30,17 @@ function stop() {
     }
 }
 function reset() {
+    document.getElementById("controls").innerHTML = /*HTML*/`
+    <button id="startBtn" onclick="start()">start</button>
+    <button id="stopBtn" onclick="stop()">stop</button>
+    <button id="resetBtn" onclick="reset()">reset</button>
+    `;
     clearInterval(timer);
     startTime = 0;
     elapsedTime = 0;
     isRunning = false;
     display.textContent = "00:00:00:00";
+    lapsContainer.innerHTML = "";
 }
 function update() {
     const currentTime = Date.now();
@@ -40,4 +57,11 @@ function update() {
     milliseconds = String(milliseconds).padStart(2, "0");
 
     display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
+function doLap() {
+    const lapTime = display.textContent;
+    const lapElement = document.createElement("div");
+    lapElement.textContent = lapTime;
+    lapsContainer.appendChild(lapElement);
+    lapsContainer.scrollTop = lapsContainer.scrollHeight;
 }
